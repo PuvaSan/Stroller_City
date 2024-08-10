e0f28ad71cb1ea864fd2d53995a26a263859e66c
Ayano Fujita <cathy.ayano@gmail.com>
2024-08-10 15:20:05 +0900

    sorry

--- app/javascript/controllers/home_map_controller.js
+++ app/javascript/controllers/home_map_controller.js
@@ -2,13 +2,14 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="another-map"
 export default class extends Controller {
-  static targets = [ "name", "address", "photo"]
+  static targets = [ "name", "address", "photo", "originInput"]
   connect() {
     console.log("home map connected")
     console.log(this.nameTarget, this.addressTarget)
   }
 
-
+  directionsService = null
+  directionsRenderer = null
 
   async initMap() {
     let map = new google.maps.Map(document.getElementById('map'),{
@@ -23,17 +24,24 @@ export default class extends Controller {
     const transitLayer = new google.maps.TransitLayer();
     transitLayer.setMap(map);
 
+    // initializes direction renderer on map
+    this.directionsService = new google.maps.DirectionsService()
+    this.directionsRenderer = new google.maps.DirectionsRenderer()
+    this.directionsRenderer.setMap(map)
+
     // autocompletes location inputs for origin and destination
     let originAutocomplete = new google.maps.places.Autocomplete(document.getElementById('origin'))
     let destinationAutocomplete = new google.maps.places.Autocomplete(document.getElementById('destination'))
 
-    // recenters map when input is changed to a google place
     destinationAutocomplete.addListener('place_changed', () => {
       let place = destinationAutocomplete.getPlace();
+
+      // recenters map when input is changed to a google place
       if (place.geometry && place.geometry.location) {
         map.setCenter(place.geometry.location);
         map.setZoom(15);
 
+        // displays place name, address, and photos
         this.nameTarget.innerText = place.name;
         this.addressTarget.innerText = place.formatted_address;
         this.photoTarget.innerHTML = "";
@@ -42,10 +50,12 @@ export default class extends Controller {
           const imgElement = `<img height=100 class="m-3" src="${placeImage}" />`;
           this.photoTarget.insertAdjacentHTML("beforeend", imgElement);
         });
-        document.querySelector("#draggable-panel").style.height = "90vh";
+        document.querySelector("#draggable-panel").style.height = "80vh";
         document.querySelector("#draggable-panel").style.borderRadius = "0px";
         document.querySelector("#initial-content").outerHTML = "";
+        document.querySelector("#directions-button").classList.toggle("d-none");
         console.log(place.name)
+
         // Send the place name to Rails controller via AJAX
         if (place.name) {
           fetch('/pages/receive_place_name', {
@@ -77,6 +87,38 @@ export default class extends Controller {
         }
       }
     });
+  }
 
+  showStuff() {
+    this.originInputTarget.classList.toggle("d-none")
+    document.getElementById("go-button").classList.toggle("d-none")
+  }
+
+  direct() {
+    // DONT REMOVE THESE, IT'S FOR FUTURE IMPLEMENTATION
+    // map renderer
+    // this.directionsService.route({
+    //   origin: document.getElementById('origin').value,
+    //   destination: document.getElementById('destination').value,
+    //   travelMode: 'WALKING'
+    // }, (response, status) => {
+    //   if(status === 'OK') {
+    //     console.log(response)
+    //     this.directionsRenderer.setDirections(response);
+    //   } else {
+    //     window.alert('Directions request failed due to ' + status);
+    //   }
+    // });
+
+    // displays directions panel
+    // let panel = document.getElementById('directions-panel');
+    // this.directionsRenderer.setPanel(panel);
+
+    //route index
+    fetch(`/routes`)
+      .then(response => response.text())
+      .then(html => {
+        document.getElementById('panel-content').innerHTML = html;
+      });
   }
 }
