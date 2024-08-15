3d11f3185058e98f9225b2b8e660495b7b71b376
ikumi ojiro <ikumidudecool@gmail.com>
2024-08-15 20:40:28 +0900

    recent

--- app/javascript/controllers/home_map_controller.js
+++ app/javascript/controllers/home_map_controller.js
@@ -6,18 +6,16 @@ export default class extends Controller {
   connect() {
     console.log("home map connected")
     console.log(this.nameTarget, this.addressTarget)
-    document.querySelector("#draggable-panel").style.height = "92vh";
-    //get recent search history from local storage
-    const recent = JSON.parse(localStorage.getItem('recent'))
-    recent.forEach(place => {
-    this.recentTarget.insertAdjacentHTML("beforeend", `<li>${place}</li>`)
-    })
-    //iterate over search history
-    //on each iteration, insert an LI element into UL target
+    document.querySelector("#draggable-panel").style.height = "80vh";
+        //get recent search history from local storage
+        const recent = JSON.parse(localStorage.getItem('recent'))
+        recent.slice(Math.max(recent.length - 5, 0)).forEach(place => {
+        this.recentTarget.insertAdjacentHTML("beforeend", `<li>${place}</li>`)
+        })
   }
 
-  directionsService = null
-  directionsRenderer = null
+  originAutocomplete = null;
+  destinationAutocomplete = null;
 
   async initMap() {
     let map = new google.maps.Map(document.getElementById('map'),{
@@ -32,22 +30,17 @@ export default class extends Controller {
     const transitLayer = new google.maps.TransitLayer();
     transitLayer.setMap(map);
 
-    // initializes direction renderer on map
-    this.directionsService = new google.maps.DirectionsService()
-    this.directionsRenderer = new google.maps.DirectionsRenderer()
-    this.directionsRenderer.setMap(map)
-
     // autocompletes location inputs for origin and destination
-    let originAutocomplete = new google.maps.places.Autocomplete(document.getElementById('origin'))
-    let destinationAutocomplete = new google.maps.places.Autocomplete(document.getElementById('destination'))
+    this.originAutocomplete = new google.maps.places.Autocomplete(document.getElementById('origin'))
+    this.destinationAutocomplete = new google.maps.places.Autocomplete(document.getElementById('destination'))
 
     var paragraphs = document.querySelectorAll('#description');
     paragraphs.forEach(function(p) {
       p.style.fontSize = '10px';
     });
 
-    destinationAutocomplete.addListener('place_changed', () => {
-      let place = destinationAutocomplete.getPlace();
+    this.destinationAutocomplete.addListener('place_changed', () => {
+      let place = this.destinationAutocomplete.getPlace();
 
       // recenters map when input is changed to a google place
       if (place.geometry && place.geometry.location) {
@@ -67,29 +60,20 @@ export default class extends Controller {
         });
         document.querySelector("#draggable-panel").style.height = "80vh";
         document.querySelector("#draggable-panel").style.borderRadius = "0px";
-        document.querySelector("#initial-content").classList.add("d-none");
-        document.querySelectorAll("#icon").forEach(element => {
-          element.classList.toggle("d-none");
-        });
-        document.querySelector("#directions-button").classList.toggle("d-none");
+        document.querySelector("#initial-content").outerHTML = "";
+        document.querySelector("#place-description").classList.toggle("d-none");
+        document.querySelector("#reviews-container").classList.toggle("d-none");
+        console.log(place.name)
 
-      //save place to recent array
-
-      if (localStorage.getItem('recent') === null) {
-        let recent = [];
-        recent.push(place.name);
-        localStorage.setItem('recent', JSON.stringify(recent));
-      } else {
-        let recent = JSON.parse(localStorage.getItem('recent'));
-        recent.push(place.name);
-        if (recent.length > 5) {
-          recent.shift();
+        if (localStorage.getItem('recent') === null) {
+          let recent = [];
+          recent.push(place.name);
+          localStorage.setItem('recent', JSON.stringify(recent));
+        } else {
+          let recent = JSON.parse(localStorage.getItem('recent'));
+          recent.push(place.name);
+          localStorage.setItem('recent', JSON.stringify(recent));
         }
-        localStorage.setItem('recent', JSON.stringify(recent));
-      }
-
-      console.log(JSON.parse(localStorage.getItem('recent')));
-    }
 
         // Send the place name to Rails controller via AJAX
         if (place.name) {
@@ -130,30 +114,42 @@ export default class extends Controller {
   }
 
   direct() {
-    // DONT REMOVE THESE, IT'S FOR FUTURE IMPLEMENTATION
-    // map renderer
-    // this.directionsService.route({
-    //   origin: document.getElementById('origin').value,
-    //   destination: document.getElementById('destination').value,
-    //   travelMode: 'WALKING'
-    // }, (response, status) => {
-    //   if(status === 'OK') {
-    //     console.log(response)
-    //     this.directionsRenderer.setDirections(response);
-    //   } else {
-    //     window.alert('Directions request failed due to ' + status);
-    //   }
-    // });
+    const currentDateTime = new Date().toISOString().replace(/:/g, '%3A').split('.')[0];
+    const origin = this.originAutocomplete.getPlace();
+    const start_lat = origin.geometry.location.lat();
+    const start_long = origin.geometry.location.lng();
+    const destination = this.destinationAutocomplete.getPlace();
+    const end_lat = destination.geometry.location.lat();
+    const end_long = destination.geometry.location.lng();
 
-    // displays directions panel
-    // let panel = document.getElementById('directions-panel');
-    // this.directionsRenderer.setPanel(panel);
+    // Use the coordinates to construct the URL for the API request
+    const url = `https://navitime-route-totalnavi.p.rapidapi.com/route_transit?start=${start_lat}%2C${start_long}&goal=${end_lat}%2C${end_long}&datum=wgs84&term=1440&walk_route=babycar&limit=5&start_time=${currentDateTime}&coord_unit=degree`;
+    console.log(url);
 
-    //route index
-    fetch(`/routes`)
-      .then(response => response.text())
-      .then(html => {
-        document.getElementById('panel-content').innerHTML = html;
-      });
+    // Make the API request
+    fetch(url, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'x-rapidapi-key': 'ee4ee6a653msh2bda56689a8b341p1b0f92jsnc9c233194414',
+        'x-rapidapi-host': 'navitime-route-totalnavi.p.rapidapi.com',
+      },
+    })
+    .then(response => response.json())
+    .then(data => {
+      // Handle the response data
+      console.log(data);
+
+      // Construct the Rails route URL with query parameters
+      const redirectUrl = `/routes?start_lat=${start_lat}&start_long=${start_long}&end_lat=${end_lat}&end_long=${end_long}`;
+
+      // Redirect to the constructed URL
+      window.location.href = redirectUrl;
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+    });
   }
+
 }
