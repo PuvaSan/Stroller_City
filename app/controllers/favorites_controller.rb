34fcaae13d8df7aea992157e24a85688377845e5
Javier Ruiz <ruizjavi070@gmail.com>
2024-08-16 21:40:14 +0900

    comitting before pulling

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,12 @@
 class FavoritesController < ApplicationController
+  before_action :authenticate_user!
   def index
     @reviews = Review.all
     @review = Review.new
+  end
+
+  def my_list
+    @reviews = current_user.reviews.includes(:place) # Assuming each review belongs to a user and a place
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,9 @@ Rails.application.routes.draw do
   #this sends the api placename to the controller
   get 'pages/render_reviews', to: 'pages#render_reviews'
 
+  get 'my_list', to: 'favorites#my_list', as: 'my_list'
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
