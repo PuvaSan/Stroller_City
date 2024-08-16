85d8e4a1bdb83a59620817fb4a2e79be26e97382
Brian Inphouva <152071631+PuvaSan@users.noreply.github.com>
2024-08-16 17:09:16 +0900

    making adjustments to show page to be more legible

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -34,53 +34,7 @@ export default class extends Controller {
     // Add transit layer
     const transitLayer = new google.maps.TransitLayer();
     transitLayer.setMap(map);
-
-    // Plot routes from Navitime response
-    this.plotRoutes(map);
   }
 
-  plotRoutes(map) {
-    if (!this.navitimeResponseValue || !this.navitimeResponseValue.items) {
-      console.error("No valid routes to display.");
-      console.log("Navitime Response:", this.navitimeResponseValue); // Debugging line
-      return;
-    }
 
-    this.navitimeResponseValue.items.forEach(item => {
-      console.log("Plotting Route Item:", item); // Debugging line
-      const routePath = new google.maps.Polyline({
-        path: item.shapes.features.flatMap(feature => feature.geometry.coordinates.map(coord => ({ lat: coord[1], lng: coord[0] }))),
-        geodesic: true,
-        strokeColor: '#00FF00', // Change to green
-        strokeOpacity: 1.0,
-        strokeWeight: 4
-      });
-
-      routePath.setMap(map);
-      console.log("Route path added to the map"); // Debugging line
-
-      // Add markers for start and end points
-      const start = item.summary.start.coord;
-      const goal = item.summary.goal.coord;
-
-      this.addMarker(map, start.lat, start.lon, item.summary.start.name);
-      this.addMarker(map, goal.lat, goal.lon, item.summary.goal.name);
-    });
-  }
-
-  addMarker(map, lat, lng, title) {
-    const marker = new google.maps.Marker({
-      position: { lat, lng },
-      map: map,
-      title: title
-    });
-
-    const infoWindow = new google.maps.InfoWindow({
-      content: title
-    });
-
-    marker.addListener('click', () => {
-      infoWindow.open(map, marker);
-    });
-  }
 }
