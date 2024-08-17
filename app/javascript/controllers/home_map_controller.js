9e2422063770c3a1431e20238ec7a284473a37d7
Ayano Fujita <cathy.ayano@gmail.com>
2024-08-17 17:31:39 +0900

    styled maps, added back button, implemented input feedback

--- app/javascript/controllers/home_map_controller.js
+++ app/javascript/controllers/home_map_controller.js
@@ -31,10 +31,13 @@ export default class extends Controller {
   originAutocomplete = null;
   destinationAutocomplete = null;
 
+  origin = null;
+
   async initMap() {
     let map = new google.maps.Map(document.getElementById('map'),{
       center:{lat:35.652832,lng:139.839478},
-      zoom:13
+      zoom:13,
+      disableDefaultUI: true,
     })
     google.maps.event.addListener(map,"click",function(event) {
       this.setOptions({scrollwheel:true})
@@ -44,20 +47,132 @@ export default class extends Controller {
     const transitLayer = new google.maps.TransitLayer();
     transitLayer.setMap(map);
 
+    //add retro style
+    map.setOptions({ styles: [
+      { elementType: "geometry", stylers: [{ color: "#ebe3cd" }] },
+      { elementType: "labels.text.fill", stylers: [{ color: "#523735" }] },
+      { elementType: "labels.text.stroke", stylers: [{ color: "#f5f1e6" }] },
+      {
+        featureType: "administrative",
+        elementType: "geometry.stroke",
+        stylers: [{ color: "#c9b2a6" }],
+      },
+      {
+        featureType: "administrative.land_parcel",
+        elementType: "geometry.stroke",
+        stylers: [{ color: "#dcd2be" }],
+      },
+      {
+        featureType: "administrative.land_parcel",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#ae9e90" }],
+      },
+      {
+        featureType: "landscape.natural",
+        elementType: "geometry",
+        stylers: [{ color: "#dfd2ae" }],
+      },
+      {
+        featureType: "poi",
+        elementType: "geometry",
+        stylers: [{ color: "#dfd2ae" }],
+      },
+      {
+        featureType: "poi",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#93817c" }],
+      },
+      {
+        featureType: "poi.park",
+        elementType: "geometry.fill",
+        stylers: [{ color: "#a5b076" }],
+      },
+      {
+        featureType: "poi.park",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#447530" }],
+      },
+      {
+        featureType: "road",
+        elementType: "geometry",
+        stylers: [{ color: "#f5f1e6" }],
+      },
+      {
+        featureType: "road.arterial",
+        elementType: "geometry",
+        stylers: [{ color: "#fdfcf8" }],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry",
+        stylers: [{ color: "#f8c967" }],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry.stroke",
+        stylers: [{ color: "#e9bc62" }],
+      },
+      {
+        featureType: "road.highway.controlled_access",
+        elementType: "geometry",
+        stylers: [{ color: "#e98d58" }],
+      },
+      {
+        featureType: "road.highway.controlled_access",
+        elementType: "geometry.stroke",
+        stylers: [{ color: "#db8555" }],
+      },
+      {
+        featureType: "road.local",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#806b63" }],
+      },
+      {
+        featureType: "transit.line",
+        elementType: "geometry",
+        stylers: [{ color: "#dfd2ae" }],
+      },
+      {
+        featureType: "transit.line",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#8f7d77" }],
+      },
+      {
+        featureType: "transit.line",
+        elementType: "labels.text.stroke",
+        stylers: [{ color: "#ebe3cd" }],
+      },
+      {
+        featureType: "transit.station",
+        elementType: "geometry",
+        stylers: [{ color: "#dfd2ae" }],
+      },
+      {
+        featureType: "water",
+        elementType: "geometry.fill",
+        stylers: [{ color: "#b9d3c2" }],
+      },
+      {
+        featureType: "water",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#92998d" }],
+      },
+    ] });
+
     // autocompletes location inputs for origin and destination
+    this.originAutocomplete = new google.maps.places.Autocomplete(document.getElementById('origin'))
     this.destinationAutocomplete = new google.maps.places.Autocomplete(document.getElementById('destination'))
 
-    // var paragraphs = document.querySelectorAll('#description');
-    // paragraphs.forEach(function(p) {
-    //   p.style.fontSize = '10px';
-    // });
+    this.originAutocomplete.addListener('place_changed', () => {
+      this.origin = this.originAutocomplete.getPlace();
+    });
 
     this.destinationAutocomplete.addListener('place_changed', () => {
       let place = this.destinationAutocomplete.getPlace();
 
       // recenters map when input is changed to a google place
       if (place.geometry && place.geometry.location) {
-        map.setCenter(place.geometry.location);
+        map.setCenter( { lat: place.geometry.location.lat() - 0.005, lng: place.geometry.location.lng() });
         map.setZoom(15);
         // Add marker to the selected place
         const marker = new google.maps.Marker({
@@ -65,6 +180,8 @@ export default class extends Controller {
           map: map,
           title: place.name
         });
+
+        this.getCurrentPosition();
 
         // displays place name, address, and photos
         this.nameTarget.innerText = place.name;
@@ -79,10 +196,11 @@ export default class extends Controller {
           const imgElement = `<img height=80 width=80 class="me-2" src="${placeImage}" />`;
           this.photoTarget.insertAdjacentHTML("beforeend", imgElement);
         });
-        document.querySelector("#draggable-panel").style.height = "fit-content";
-        document.querySelector("#initial-content").outerHTML = "";
+        document.querySelector("#draggable-panel").style.height = "350px";
+        document.querySelector("#initial-content").classList.toggle("d-none");
         document.querySelector("#place-description").classList.toggle("d-none");
         document.querySelector("#reviews-container").classList.toggle("d-none");
+        document.getElementById("first-back-button").classList.toggle("d-none");
 
         if (localStorage.getItem('recent') === null) {
           let recent = [];
@@ -125,66 +243,61 @@ export default class extends Controller {
           .catch(error => {
             console.error('There was a problem with the fetch operation:', error);
           });
-
         }
       }
     });
   }
 
+  user_lat = null;
+  user_long = null;
   getCurrentPosition() {
     //sets origin to user's current location
     navigator.geolocation.getCurrentPosition((position) => {
-      let user_lat = position.coords.latitude;
-      let user_long = position.coords.longitude;
-      fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${user_lat},${user_long}`)
+      this.user_lat = position.coords.latitude;
+      this.user_long = position.coords.longitude;
+      fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${this.user_lat},${this.user_long}&key=AIzaSyA4qrApURx9lwvAae-pPmNbV07vPqlbHxo`)
         .then(response => response.json())
         .then(data => {
-          console.log(data)
+          document.getElementById('origin').value = data.results[0].formatted_address
         })
     })
+    this.origin = undefined;
+  }
 
-    // this.userLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-    // document.getElementById('origin').value = (poition.coords.latitude + ", " + position.coords.longitude);
-    // this.originAutocomplete = new google.maps.places.Autocomplete(document.getElementById('origin'))
+  firstBack() {
+    document.querySelector("#draggable-panel").style.height = "80vh";
+    document.querySelector("#initial-content").classList.toggle("d-none");
+    document.querySelector("#place-description").classList.toggle("d-none");
+    document.querySelector("#reviews-container").classList.toggle("d-none");
+    document.getElementById("first-back-button").classList.toggle("d-none");
+    this.originInputTarget.classList.toggle("d-none")
+    document.getElementById("destination").parentElement.classList.toggle("d-none")
   }
 
   direct() {
     const currentDateTime = new Date().toISOString().replace(/:/g, '%3A').split('.')[0];
-    const origin = this.originAutocomplete.getPlace();
-    const start_lat = origin.geometry.location.lat();
-    const start_long = origin.geometry.location.lng();
+    let start_lat = 0;
+    let start_long = 0;
+    if (this.origin) {
+      start_lat = this.origin.geometry.location.lat();
+      start_long = this.origin.geometry.location.lng();
+    }
+    else {
+      start_lat = this.user_lat;
+      start_long = this.user_long;
+      this.origin = { name: "Your current location" };
+    }
+    console.log(start_lat)
+
     const destination = this.destinationAutocomplete.getPlace();
     const end_lat = destination.geometry.location.lat();
     const end_long = destination.geometry.location.lng();
 
-    // Use the coordinates to construct the URL for the API request
-    const url = `https://navitime-route-totalnavi.p.rapidapi.com/route_transit?start=${start_lat}%2C${start_long}&goal=${end_lat}%2C${end_long}&datum=wgs84&term=1440&walk_route=babycar&limit=5&start_time=${currentDateTime}&coord_unit=degree`;
-    console.log(url);
-
-    // Make the API request
-    fetch(url, {
-      method: 'GET',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'x-rapidapi-key': 'ee4ee6a653msh2bda56689a8b341p1b0f92jsnc9c233194414',
-        'x-rapidapi-host': 'navitime-route-totalnavi.p.rapidapi.com',
-      },
-    })
-    .then(response => response.json())
-    .then(data => {
-      // Handle the response data
-      console.log(data);
-
       // Construct the Rails route URL with query parameters
-      const redirectUrl = `/routes?start_lat=${start_lat}&start_long=${start_long}&end_lat=${end_lat}&end_long=${end_long}&origin=${origin.name}&destination=${destination.name}`;
+    const redirectUrl = `/routes?start_lat=${start_lat}&start_long=${start_long}&end_lat=${end_lat}&end_long=${end_long}&origin=${this.origin.name}&destination=${destination.name}`;
 
-      // Redirect to the constructed URL
-      window.location.href = redirectUrl;
-    })
-    .catch(error => {
-      console.error('Error fetching data:', error);
-    });
+    // Redirect to the constructed URL
+    window.location.href = redirectUrl;
   }
 
 }
