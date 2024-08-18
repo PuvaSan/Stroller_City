32b299f983851d6631861c078fe37870482283da
Brian Inphouva <152071631+PuvaSan@users.noreply.github.com>
2024-08-18 14:22:12 +0900

    resized the map to fit half and the bottom panel is not adjustable, just static at half

--- app/javascript/controllers/routes_map_controller.js
+++ app/javascript/controllers/routes_map_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["content", "map"]
+  static targets = ["content", "map", "coordinates"]
 
   connect() {
     console.log("Routes map controller connected");
