b46a65b47435c99daeb701a427aaf076342b0621
Brian Inphouva <152071631+PuvaSan@users.noreply.github.com>
2024-08-20 22:42:23 +0900

    happy with color set for walk and train route, retro map also set for map screen and removed defaults, but added zoom and street view controls

--- app/javascript/controllers/routes_map_controller.js
+++ app/javascript/controllers/routes_map_controller.js
@@ -13,14 +13,139 @@ export default class extends Controller {
       zoom: 11,
       center: { lat: 35.690916, lng: 139.785495 },
       mapTypeId: "terrain",
+      disableDefaultUI: true,
+      zoomControl: true,
+      zoomControlOptions: {
+        position: google.maps.ControlPosition.RIGHT_BOTTOM,
+      },
+      streetViewControl: true,
     };
 
     this.map = new google.maps.Map(this.mapTarget, mapOptions);
     console.log("[initMap] Map initialized with center:", mapOptions.center);
 
+    // Get the coordinates for walk sections and draws the full polyline route
     this.walkSections = this.getCoordinatesForWalkSections();
     this.drawFullPolylineRoute();
+
+    // adds transit layer over our map
+    const transitLayer = new google.maps.TransitLayer();
+    transitLayer.setMap(map);
+
+    // adds retro style to our map
+    this.map.setOptions({ styles: [
+      { elementType: "geometry", stylers: [{ color: "#ebe3cd" }] },
+      { elementType: "labels.text.fill", stylers: [{ color: "#523735" }] },
+      { elementType: "labels.text.stroke", stylers: [{ color: "#f5f1e6" }] },
+      {
+        featureType: "administrative",
+        elementType: "geometry.stroke",
+        stylers: [{ color: "#c9b2a6" }],
+      },
+      {
+        featureType: "administrative.land_parcel",
+        elementType: "geometry.stroke",
+        stylers: [{ color: "#dcd2be" }],
+      },
+      {
+        featureType: "administrative.land_parcel",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#ae9e90" }],
+      },
+      {
+        featureType: "landscape.natural",
+        elementType: "geometry",
+        stylers: [{ color: "#dfd2ae" }],
+      },
+      {
+        featureType: "poi",
+        elementType: "geometry",
+        stylers: [{ color: "#dfd2ae" }],
+      },
+      {
+        featureType: "poi",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#93817c" }],
+      },
+      {
+        featureType: "poi.park",
+        elementType: "geometry.fill",
+        stylers: [{ color: "#a5b076" }],
+      },
+      {
+        featureType: "poi.park",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#447530" }],
+      },
+      {
+        featureType: "road",
+        elementType: "geometry",
+        stylers: [{ color: "#f5f1e6" }],
+      },
+      {
+        featureType: "road.arterial",
+        elementType: "geometry",
+        stylers: [{ color: "#fdfcf8" }],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry",
+        stylers: [{ color: "#f8c967" }],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry.stroke",
+        stylers: [{ color: "#e9bc62" }],
+      },
+      {
+        featureType: "road.highway.controlled_access",
+        elementType: "geometry",
+        stylers: [{ color: "#e98d58" }],
+      },
+      {
+        featureType: "road.highway.controlled_access",
+        elementType: "geometry.stroke",
+        stylers: [{ color: "#db8555" }],
+      },
+      {
+        featureType: "road.local",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#806b63" }],
+      },
+      {
+        featureType: "transit.line",
+        elementType: "geometry",
+        stylers: [{ color: "#dfd2ae" }],
+      },
+      {
+        featureType: "transit.line",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#8f7d77" }],
+      },
+      {
+        featureType: "transit.line",
+        elementType: "labels.text.stroke",
+        stylers: [{ color: "#ebe3cd" }],
+      },
+      {
+        featureType: "transit.station",
+        elementType: "geometry",
+        stylers: [{ color: "#dfd2ae" }],
+      },
+      {
+        featureType: "water",
+        elementType: "geometry.fill",
+        stylers: [{ color: "#b9d3c2" }],
+      },
+      {
+        featureType: "water",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#92998d" }],
+      },
+    ] });
+
   }
+
 
   getCoordinatesForWalkSections() {
     const routeData = this.element.dataset.route;
@@ -135,8 +260,8 @@ handleCardClick(event) {
 
     const polylineOptions = {
       path: pathCoordinates,
-      strokeOpacity: isWalking ? 0 : 0.5,
-      strokeWeight: 10,
+      strokeOpacity: isWalking ? 0 : 0.7,
+      strokeWeight: 8,
       map: this.map,
       strokeLinecap: properties.inline.strokelinecap || 'round',
       strokeLinejoin: properties.inline.strokelinejoin || 'round',
@@ -146,8 +271,8 @@ handleCardClick(event) {
       const lineSymbol = {
         path: 'M 0,-1 0,1',
         scale: 4,
-        strokeColor: properties.inline.color,
-        strokeOpacity: 0.5,
+        strokeColor: '#D81E5B', // Raspberry Set the color for walking lines
+        strokeOpacity: 0.7,
       };
 
       polylineOptions.icons = [{
@@ -156,7 +281,7 @@ handleCardClick(event) {
         repeat: '20px'
       }];
     } else {
-      polylineOptions.strokeColor = properties.inline.color;
+      polylineOptions.strokeColor = "#2364AA"; //GreenBlue Set the color for transport lines
     }
 
     const polyline = new google.maps.Polyline(polylineOptions);
@@ -206,4 +331,5 @@ handleCardClick(event) {
     console.log(`[getCoordinatesForMoveType] Found ${coordinates.length} coordinates for move type: ${moveType}`);
     return coordinates;
   }
+
 }
