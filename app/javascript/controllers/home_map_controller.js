d06930c0c48a7485bbf3904d2f92a0ea2493d796
ikumi ojiro <ikumidudecool@gmail.com>
2024-08-27 22:04:13 +0900

    fixapi

--- app/javascript/controllers/home_map_controller.js
+++ app/javascript/controllers/home_map_controller.js
@@ -5,34 +5,50 @@ export default class extends Controller {
   static targets = [ "name", "address", "photo", "originInput", "phone", "info", "recent", "recommended","recent"]
   connect() {
     console.log("home map connected")
+    console.log(this.nameTarget, this.addressTarget)
     //get recent search history from local storage
     const recent = JSON.parse(localStorage.getItem('recent'))
     recent.slice(Math.max(recent.length - 5, 0)).forEach(place => {
               this.recentTarget.insertAdjacentHTML("beforeend", `<div id="${place.id}" data-action="click->home-map#javi" class="card-category me-3" style="width: 180px; background-image: linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.3)), url(${place.photo.trim()})">${place.name}</div>`);
             });
     document.getElementById("current-location").innerText = this.getCurrentPosition();
-    this.initMap();
-    document.getElementById('tags-container').innerHTML = '';
-    let modalInputs = document.querySelectorAll('.modal-body input');
-    modalInputs.forEach(input => {
-      input.checked = false;
-    });
-    document.getElementById('hiddenPlaceId').value = "";
+    // console.log("api key", this.apiKeyValue)
+
   }
 
-  apiKey = "AIzaSyCWOSZTJ-G738Y4qoVuyVHh1YYjtWUSlao";
+  apiKey = "AIzaSyA4qrApURx9lwvAae-pPmNbV07vPqlbHxo";
 
   //Switches the Ikumi container based on the button clicked
   ikumibutton(event) {
     const buttonId = event.currentTarget.id;
-    const selectedContainer = document.getElementById(`${buttonId}-container`);
+    conAIzaSyA4qrApURx9lwvAae-pPmNbV07vPqlbHxoentById(`${buttonId}-container`);
     const containers = document.querySelectorAll(".ikumi-container > *");
+    console.log(buttonId, selectedContainer, containers);
     containers.forEach(container => {
       if (!container.classList.contains("d-none")) {
         container.classList.add("d-none");
       }
     });
     selectedContainer.classList.remove("d-none");
+  }
+
+
+  select(event) {
+    // Remove the 'selected' class from all buttons
+    this.element.querySelectorAll('.animated-button').forEach(button => {
+        button.classList.remove('selected');
+    });
+
+    // Add the 'selected' class to the clicked button
+    event.target.classList.add('selected');
+    console.log(this.element.querySelectorAll('.animated-button'))
+  }
+
+  //Makes the list of recent searches and recommended clickable
+  clickable(event) {
+    const place = event.currentTarget.innerText;
+    this.destinationAutocomplete.set('place', place);
+    console.log(place);
   }
 
   originAutocomplete = null;
@@ -42,7 +58,7 @@ export default class extends Controller {
 
   map = null;
 
-  initMap() {
+  async initMap() {
     this.map = new google.maps.Map(document.getElementById('map'),{
       center:{lat:35.652832,lng:139.839478},
       zoom:13,
@@ -69,9 +85,10 @@ export default class extends Controller {
 
     this.destinationAutocomplete.addListener('place_changed', () => {
       let place = this.destinationAutocomplete.getPlace();
+
       // recenters map when input is changed to a google place
       if (place.geometry && place.geometry.location) {
-        this.map.setCenter( place.geometry.location );
+        this.map.setCenter( { lat: place.geometry.location.lat() - 0.005, lng: place.geometry.location.lng() });
         this.map.setZoom(15);
         // Add marker to the selected place
         const marker = new google.maps.Marker({
@@ -96,10 +113,12 @@ export default class extends Controller {
           const imgElement = `<img height=80 width=80 class="me-2" src="${placeImage}" />`;
           this.photoTarget.insertAdjacentHTML("beforeend", imgElement);
         });
+        console.log(this.placePhotoContainerTarget)
         document.querySelector("#draggable-panel").style.height = "350px";
         document.querySelector("#draggable-panel").style.borderRadius = "16px 16px 0 0";
         document.querySelector("#initial-content").classList.toggle("d-none");
         document.querySelector("#place-description").classList.toggle("d-none");
+        document.querySelector("#reviews-container").classList.toggle("d-none");
         document.getElementById("first-back-button").classList.toggle("d-none");
         this.originInputTarget.classList.toggle("d-none")
         document.getElementById("destination").parentElement.classList.add("d-none")
@@ -112,6 +131,8 @@ export default class extends Controller {
           let recent = JSON.parse(localStorage.getItem('recent'));
           recent.push({ name: place.name, photo: place.photos[0].getUrl(), id: place.place_id });
           localStorage.setItem('recent', JSON.stringify(recent));
+          console.log(recent);
+          console.log(place.place_id);
         }
 
         // Send the place name to Rails controller via AJAX
@@ -134,25 +155,8 @@ export default class extends Controller {
                 .then(html => {
                   document.getElementById('reviews-container').innerHTML = html;
                 });
-
-              fetch(`/pages/render_tags?id=${data.id}`)
-                .then(response => response.text())
-                  .then(html => {
-                    document.getElementById('tags-container').insertAdjacentHTML("afterbegin", html);
-                    let modalInputs = document.querySelectorAll('.modal-body input');
-                    let placeTags = [...document.querySelectorAll('.aya-tags')].map(tag => tag.innerText);
-                    modalInputs.forEach(input => {
-                      if (placeTags.includes(input.name)) {
-                        input.checked = true;
-                      }
-                    });
-                    document.getElementById("placeTagsForm").action = `/places/${data.id}`;
-                    document.getElementById('hiddenPlaceId').value = data.id;
-                  });
             } else {
               console.error("Error:", data.message);
-              document.getElementById("placeTagsForm").action = `/places`;
-              document.getElementById('hiddenPlaceId').value = place.place_id;
             }
           })
           .catch(error => {
@@ -175,8 +179,8 @@ export default class extends Controller {
         .then(data => {
           document.getElementById('origin').value = data.results[0].formatted_address
           const addressComponents = data.results[0].address_components;
-          const localityComponent = addressComponents.find(component => component.types.includes('locality'));;
-          document.getElementById("current-location").innerText = localityComponent.long_name;
+          const localityComponent = addressComponents.find(component => component.types.includes('locality'));
+          return localityComponent.long_name;
         })
     })
     this.origin = undefined;
@@ -185,20 +189,15 @@ export default class extends Controller {
   firstBack() {
     document.querySelector("#initial-content").classList.toggle("d-none");
     document.querySelector("#place-description").classList.toggle("d-none");
-    document.querySelector("#reviews-container").classList.add("d-none");
+    document.querySelector("#reviews-container").classList.toggle("d-none");
     document.getElementById("first-back-button").classList.toggle("d-none");
     this.originInputTarget.classList.toggle("d-none")
     document.getElementById("destination").parentElement.classList.toggle("d-none")
-    this.destinationAutocomplete = new google.maps.places.Autocomplete(document.getElementById('destination'))
-    document.getElementById('tags-container').innerHTML = '';
-    let modalInputs = document.querySelectorAll('.modal-body input');
-    modalInputs.forEach(input => {
-      input.checked = false;
-    });
-    document.getElementById('hiddenPlaceId').value = "";
+    // Remove marker from the map
+    marker.setMap(null);
   }
 
-  direct(event) {
+  direct() {
     const currentDateTime = new Date().toISOString().replace(/:/g, '%3A').split('.')[0];
     let start_lat = 0;
     let start_long = 0;
@@ -211,27 +210,9 @@ export default class extends Controller {
       start_long = this.user_long;
       this.origin = { name: "Your current location" };
     }
+    console.log(start_lat)
 
-    let destination = this.destinationAutocomplete.getPlace();
-    if (destination === undefined) {
-      const buttonId = event.currentTarget.dataset.value;
-      // const url = `https://maps.googleapis.com/maps/api/place/details/json?fields=name%2Cphoto%2Cformatted_phone%2Cformatted_address%2Cgeometry%2Cphoto&place_id=${buttonId}&key=AIzaSyA4qrApURx9lwvAae-pPmNbV07vPqlbHxo`
-      const url = `https://places.googleapis.com/v1/places/${buttonId}?fields=displayName,formattedAddress,photos,location,nationalPhoneNumber,editorialSummary&key=${this.apiKey}`
-      fetch(url)
-        .then(response => response.json())
-        .then(data => {
-          destination = data;
-
-          const end_lat = destination.location.latitude;
-          const end_long = destination.location.longitude;
-
-            // Construct the Rails route URL with query parameters
-          const redirectUrl = `/routes?start_lat=${start_lat}&start_long=${start_long}&end_lat=${end_lat}&end_long=${end_long}&origin=${this.origin.name}&destination=${destination.displayName.text}`;
-
-          // Redirect to the constructed URL
-          window.location.href = redirectUrl;
-      });
-    }
+    const destination = this.destinationAutocomplete.getPlace();
     const end_lat = destination.geometry.location.lat();
     const end_long = destination.geometry.location.lng();
 
@@ -250,10 +231,11 @@ export default class extends Controller {
       .then(response => response.json())
       .then(data => {
         const place = data;
+        console.log(place);
 
         // recenters map when input is changed to a google place
         if (place.location) {
-          this.map.setCenter( { lat: place.location.latitude, lng: place.location.longitude });
+          this.map.setCenter( { lat: place.location.latitude - 0.005, lng: place.location.longitude });
           this.map.setZoom(15);
           // Add marker to the selected place
           const marker = new google.maps.Marker({
@@ -281,10 +263,10 @@ export default class extends Controller {
           document.querySelector("#draggable-panel").style.borderRadius = "16px 16px 0 0";
           document.querySelector("#initial-content").classList.toggle("d-none");
           document.querySelector("#place-description").classList.toggle("d-none");
+          document.querySelector("#reviews-container").classList.toggle("d-none");
           document.getElementById("first-back-button").classList.toggle("d-none");
-          document.getElementById("go-button").dataset.value = buttonId;
-          document.getElementById('destination').value = "";
-          this.destinationAutocomplete = new google.maps.places.Autocomplete(document.getElementById('destination'))
+
+          //new changes for inputs
           this.originInputTarget.classList.toggle("d-none")
           document.getElementById("destination").parentElement.classList.add("d-none")
 
@@ -308,24 +290,8 @@ export default class extends Controller {
                   .then(html => {
                     document.getElementById('reviews-container').innerHTML = html;
                   });
-                fetch(`/pages/render_tags?id=${data.id}`)
-                  .then(response => response.text())
-                  .then(html => {
-                    document.getElementById('tags-container').insertAdjacentHTML("afterbegin", html);
-                    let modalInputs = document.querySelectorAll('.modal-body input');
-                    let placeTags = [...document.querySelectorAll('.aya-tags')].map(tag => tag.innerText);
-                    modalInputs.forEach(input => {
-                      if (placeTags.includes(input.name)) {
-                        input.checked = true;
-                      }
-                    });
-                    document.getElementById("placeTagsForm").action = `/places/${data.id}`;
-                    document.getElementById('hiddenPlaceId').value = data.id;
-                  });
               } else {
                 console.error("Error:", data.message);
-                document.getElementById("placeTagsForm").action = `/places`;
-                document.getElementById('hiddenPlaceId').value = buttonId;
               }
             })
             .catch(error => {
